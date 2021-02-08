3a8ed999677db10a04b13c059afb8045e36fac70
Redha Nedjari <ned.r@hotmail.fr>
2021-02-08 17:32:50 +0100

    1 - Routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+// Components
+import Checkout from "./components/Checkout";
+import Header from "./components/Header";
+import Customize from "./components/Customize";
+// Router
+import { Switch, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <h1>Let's make some pizza</h1>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Customize />
+        </Route>
+        <Route path="/checkout">
+          <Checkout />
+        </Route>
+      </Switch>
     </div>
   );
 }
